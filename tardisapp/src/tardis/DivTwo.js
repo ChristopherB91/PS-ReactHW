b1ec3c082188fc6933693dcb6a3b4210ce7bbfc5
ChristopherB91 <87198039+ChristopherB91@users.noreply.github.com>
2021-09-03 21:03:26 -0400

    Tardis

---
+++ tardisapp/src/tardis/DivTwo.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Div3 from "./DivThree";
+
+export default class Div2 extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Div3 name={this.props.name} change={this.props.changeIt} />
+        </div>
+        <div>
+          <Div3 name={this.props.name} change={this.props.changeIt} />
+        </div>
+      </div>
+    );
+  }
+}
